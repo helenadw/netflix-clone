1b260d998ab6e5c65d057e0a63b2b5e031c97ec2
Helena DeMaria-Williams <25150955+hcdw@users.noreply.github.com>
2021-03-08 10:36:27 +0000

    added accordion component

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -2,26 +2,49 @@ import styled from 'styled-components';
 
 export const Container = styled.div `
     display: flex;
+    max-width: 1000px;
+    flex-direction: column;
+    padding: 70px 56px;
+    margin: auto;
+
+    @media (max-width: 1000px) {
+        padding: 70px 30px
+    }
 `
 ;
 export const Column = styled.div `
     display: flex;
+    flex-direction: column;
+    text-align: left;
+
+    @media (max-width: 1000px) {
+        grid-template-columns: repeat(auto-fill, minmax(150px, 1fr))
+    }
 `
 ;
 export const Row = styled.div `
-    display: flex;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+    grid-gap: 15px;
 `
 ;
 export const Link = styled.a `
-    display: flex;
+    color: #757575;
+    margin-bottom: 20px;
+    font-size: 13px;
+    text-decoration: none;
 `
 ;
 export const Title = styled.p `
-    display: flex;
+    font-size: 16px;
+    color: #757575;
+    margin-bottom: 40px;
 `
 ;
 export const Text = styled.p `
-    display: flex;
+    font-size: 13px;
+    color: #757575;
+    margin-bottom: 40px;
 `
 ;
 export const Break = styled.div `
