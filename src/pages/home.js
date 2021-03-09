9cdad3e6027feb8cdc516f80803062a610ac6224
Helena DeMaria-Williams <25150955+hcdw@users.noreply.github.com>
2021-03-09 11:26:45 +0000

    feat: feature component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Feature, OptForm } from '../components/'
 import { JumbotronContainer } from '../containers/jumbotron'
 import { FooterContainer} from '../containers/footer'
 import { FaqsContainer } from '../containers/faqs'
@@ -8,6 +9,16 @@ export default function Home() {
     return (
         <>
         <HeaderContainer>
+            <Feature>
+                <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+                <Feature.Subtitle>Watch anywhere. Cancel at any time.</Feature.Subtitle>
+                <OptForm>
+                    <OptForm.Input placeholder='Email Address'/>
+                    <OptForm.Button>Get Started</OptForm.Button>
+                    <OptForm.Text>Ready to watch? Enter your email to create or restart 
+                    your membership.</OptForm.Text>
+                </OptForm>
+            </Feature>
         </HeaderContainer>
         <JumbotronContainer />
         <FaqsContainer />
