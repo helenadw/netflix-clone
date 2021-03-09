460ca389012126891610b5551fa5734a56d8a8e8
Helena DeMaria-Williams <25150955+hcdw@users.noreply.github.com>
2021-03-09 09:06:18 +0000

    FEAT: header component

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { JumbotronContainer } from '../containers/jumbotron'
 import { FooterContainer} from '../containers/footer'
 import { FaqsContainer } from '../containers/faqs'
+import { HeaderContainer } from '../containers/header'
 
 export default function Home() {
     return (
         <>
+        <HeaderContainer>
+        </HeaderContainer>
         <JumbotronContainer />
         <FaqsContainer />
         <FooterContainer />
