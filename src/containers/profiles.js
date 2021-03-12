98ad5b367fa191138ecec05757bb6aeb076c4d6f
Helena DeMaria-Williams <25150955+hcdw@users.noreply.github.com>
2021-03-12 10:12:01 +0000

    FEAT: profiles

---
+++ src/containers/profiles.js
@@ -0,0 +1,35 @@
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+import { Header, Profiles } from '../components'
+import * as ROUTES from '../constants/routes'
+
+export function SelectProfileContainer({ user, setProfile }) {
+    return (
+        <>
+            <Header bg={false}>
+                <Header.Frame>
+                    <Header.Logo
+                        to={ROUTES.HOME}
+                        src='/images/misc/logo.svg'
+                        alt='Netflix'
+                    />
+                </Header.Frame>
+            </Header>
+            <Profiles>
+                <Profiles.Title>Who's watching?</Profiles.Title>
+                <Profiles.List>
+                    <Profiles.User
+                        onClick={() => setProfile({
+                            displayName: user.displayName,
+                            photoUrl: user.photoUrl
+                        })}
+                    >
+                        <Profiles.Picture src={user.photoUrl} />
+                        <Profiles.Name>{user.displayName}</Profiles.Name>
+                    </Profiles.User>
+                </Profiles.List>
+                
+            </Profiles>
+        </>
+    )
+}
