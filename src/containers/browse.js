dcc503fda6ad7d349b2d48b37e5d7787f61473da
Helena DeMaria-Williams <25150955+hcdw@users.noreply.github.com>
2021-03-12 10:11:10 +0000

    FEAT: browse page

---
+++ src/containers/browse.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { Header } from '../components'
+import * as ROUTES from '../constants/routes'
+import { FirebaseContext } from '../context/firebase'
+import { SelectProfileContainer } from './profiles'
+import { FooterContainer } from './footer'
+
+export function BrowseContainer() {
+    const [profile, setProfile] = useState({})
+    const user = {
+        displayName: 'Helena',
+        photoUrl: '1'
+    }
+    return profile.displayName ? (
+        <>
+            <div>Browse Container</div>
+            <FooterContainer />
+        </>
+    ) : (<SelectProfileContainer user={user} setProfile={setProfile}/>)
+}
