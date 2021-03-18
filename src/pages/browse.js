0f3e440c01f32a994bba077d98388383ed972b8d
Helena DeMaria-Williams <25150955+hcdw@users.noreply.github.com>
2021-03-18 12:33:15 +0000

    FEAT: video player

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { BrowseContainer } from '../containers/browse'
+import { useContent } from '../hooks'
+import { selectionMap } from '../utils'
 
 export default function Browse() {
-    return <BrowseContainer />
-}
+    const { series } = useContent('series');
+    const { films } = useContent('films');
+    const slides = selectionMap({ series, films });
+
+    // console.log(slides) 
+
+    return <BrowseContainer slides={slides} />
+} 
