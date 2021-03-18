436ed6c5c6944fd25f563387fcb43ed83815a2a6
Helena DeMaria-Williams <25150955+hcdw@users.noreply.github.com>
2021-03-18 12:36:51 +0000

    UPDATE: header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -13,7 +13,10 @@ import {
     PlayButton,
     Search,
     SearchIcon,
-    SearchInput
+    SearchInput,
+    Picture,
+    Dropdown,
+    Profile
 } from './styles/header'
 
 export default function Header({ bg = true, children, ...restProps}) {
@@ -76,4 +79,16 @@ Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps})
 
 Header.PlayButton = function HeaderPlayButton({ children, ...restProps}) {
     return <PlayButton {...restProps}>{children}</PlayButton>
+}
+
+Header.Picture = function HeaderPicture({ src, ...restProps}) {
+    return <Picture {...restProps} src={`/images/users/${src}.png`} />
+}
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps}) {
+    return <Dropdown {...restProps}>{children}</Dropdown>
+}
+
+Header.Profile = function HeaderProfile({ children, ...restProps}) {
+    return <Profile {...restProps}>{children}</Profile>
 }
